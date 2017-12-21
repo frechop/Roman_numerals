82b4fca5cf6e38ab490ab19657b8cef5ff2bbcff
David <dkarapetyan94@gmail.com>
2017-12-21 22:22:05 +0200

    Fix styling add cases and unit tests

--- Roman_numerals/Program.cs
+++ Roman_numerals/Program.cs
@@ -9,16 +9,19 @@ namespace Roman_numerals
         {
             if ((number < 0) || (number > 3999)) throw new ArgumentOutOfRangeException("Inserted number is out of range");
             if (number < 1) return string.Empty;
-            if (number >= 1000) return "M" + ConvertToRoman(number - 1000);
-            if (number >= 500) return "D" + ConvertToRoman(number - 500);
-            if (number >= 100) return "C" + ConvertToRoman(number - 100);
-            if (number >= 90) return "XC" + ConvertToRoman(number - 90);
-            if (number >= 50) return "L" + ConvertToRoman(number - 50);
-            if (number >= 10) return "X" + ConvertToRoman(number - 10);
-            if (number >= 9) return "IX" + ConvertToRoman(number - 9);
-            if (number >= 5) return "V" + ConvertToRoman(number - 5);
-            if (number >= 4) return "IV" + ConvertToRoman(number - 4);
-            if (number >= 1) return "I" + ConvertToRoman(number - 1);
+            if (number >= 1000) return "M " + ConvertToRoman(number - 1000);
+            if (number >= 900) return "CM " + ConvertToRoman(number - 900);
+            if (number >= 500) return "D " + ConvertToRoman(number - 500);
+            if (number >= 400) return "CD " + ConvertToRoman(number - 400);
+            if (number >= 100) return "C " + ConvertToRoman(number - 100);
+            if (number >= 90) return "XC " + ConvertToRoman(number - 90);
+            if (number >= 50) return "L " + ConvertToRoman(number - 50);
+            if (number >= 40) return "XL " + ConvertToRoman(number - 40);
+            if (number >= 10) return "X " + ConvertToRoman(number - 10);
+            if (number >= 9) return "IX " + ConvertToRoman(number - 9);
+            if (number >= 5) return "V " + ConvertToRoman(number - 5);
+            if (number >= 4) return "IV " + ConvertToRoman(number - 4);
+            if (number >= 1) return "I " + ConvertToRoman(number - 1);
             throw new ArgumentException("Incorrect input");
         }
     }
--- UnitTestProject1/IntegerToRomanTests.cs
+++ UnitTestProject1/IntegerToRomanTests.cs
@@ -21,70 +21,106 @@ namespace Roman_Numerals_Tests
         public void ConvertToRoman_1_I()
         {
             // Act & Assert
-            Assert.AreEqual("I", Roman.ConvertToRoman(1));
+            Assert.AreEqual("I ", Roman.ConvertToRoman(1));
         }
 
         [TestMethod]
         public void ConvertToRoman_4_IV()
         {
             // Act & Assert
-            Assert.AreEqual("IV", Roman.ConvertToRoman(4));
+            Assert.AreEqual("IV ", Roman.ConvertToRoman(4));
         }
 
         [TestMethod]
         public void ConvertToRoman_5_V()
         {
             // Act & Assert
-            Assert.AreEqual("V", Roman.ConvertToRoman(5));
+            Assert.AreEqual("V ", Roman.ConvertToRoman(5));
         }
 
         [TestMethod]
         public void ConvertToRoman_9_IX()
         {
             // Act & Assert
-            Assert.AreEqual("IX", Roman.ConvertToRoman(9));
+            Assert.AreEqual("IX ", Roman.ConvertToRoman(9));
         }
 
         [TestMethod]
         public void ConvertToRoman_10_X()
         {
             // Act & Assert
-            Assert.AreEqual("X", Roman.ConvertToRoman(10));
+            Assert.AreEqual("X ", Roman.ConvertToRoman(10));
+        }
+
+        [TestMethod]
+        public void ConvertToRoman_40_XL()
+        {
+            // Act & Assert
+            Assert.AreEqual("XL ", Roman.ConvertToRoman(40));
         }
 
         [TestMethod]
         public void ConvertToRoman_50_L()
         {
             // Act & Assert
-            Assert.AreEqual("L", Roman.ConvertToRoman(50));
+            Assert.AreEqual("L ", Roman.ConvertToRoman(50));
         }
 
         [TestMethod]
         public void ConvertToRoman_90_XC()
         {
             // Act & Assert
-            Assert.AreEqual("XC", Roman.ConvertToRoman(90));
+            Assert.AreEqual("XC ", Roman.ConvertToRoman(90));
         }
 
         [TestMethod]
         public void ConverToRoman_100_C()
         {
             // Act & Assert
-            Assert.AreEqual("C", Roman.ConvertToRoman(100));
+            Assert.AreEqual("C ", Roman.ConvertToRoman(100));
+        }
+
+        [TestMethod]
+        public void ConverToRoman_400_CD()
+        {
+            // Act & Assert
+            Assert.AreEqual("CD ", Roman.ConvertToRoman(400));
         }
 
         [TestMethod]
         public void ConvertToRoman_500_D()
         {
             // Act & Assert
-            Assert.AreEqual("D", Roman.ConvertToRoman(500));
+            Assert.AreEqual("D ", Roman.ConvertToRoman(500));
+        }
+
+        [TestMethod]
+        public void ConvertToRoman_900_CM()
+        {
+            // Act & Assert
+            Assert.AreEqual("CM ", Roman.ConvertToRoman(900));
         }
 
         [TestMethod]
         public void ConvertToRoman_1000_M()
         {
             // Act & Assert
-            Assert.AreEqual("M", Roman.ConvertToRoman(1000));
+            Assert.AreEqual("M ", Roman.ConvertToRoman(1000));
         }
+
+        [TestMethod]
+        public void ConvertToRoman_444_CDXLIV()
+        {
+            // Act & Assert
+            Assert.AreEqual("CD XL IV ", Roman.ConvertToRoman(444));
+        }
+
+        [TestMethod]
+        public void ConvertToRoman_3999_MMMCMXCIX()
+        {
+            // Act & Assert
+            Assert.AreEqual("M M M CM XC IX ", Roman.ConvertToRoman(3999));
+        }
+
     }
 }
