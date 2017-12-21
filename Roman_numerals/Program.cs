6dce17c5d81e00594280fe102f45736bde5a06fd
David <dkarapetyan94@gmail.com>
2017-12-21 21:07:22 +0200

    Add project files.

---
+++ Roman_numerals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Roman_numerals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
